88589dd02e68f89c8c3c4601609ae355fdc29141
GPapu <96311843+GPapu@users.noreply.github.com>
2022-01-16 15:48:35 +0400

    add new HomeWork4

---
+++ newHomeWork4/newHomeWork4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace newHomeWork4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("giorgi papuashvili");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
